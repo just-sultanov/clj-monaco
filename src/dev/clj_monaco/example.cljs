158ab7ba8f515919f74ce53d820db89dfea9f354
Ilshat Sultanov <just.sultanov@gmail.com>
2020-01-15 15:46:06 +0300

    added simple styles

--- src/dev/clj_monaco/example.cljs
+++ src/dev/clj_monaco/example.cljs
@@ -26,19 +26,48 @@
   (fn [db]
     (:text db)))
 
+(def default-props
+  {:width            "100%",
+   :height           "100%",
+   :value            "(+ 1 2 3)",
+   :defaultValue     "",
+   :language         "clojure",
+   :theme            "vs",
+   :options          {},
+   :overrideServices {},
+   :editorDidMount   identity,
+   :editorWillMount  identity,
+   :onChange         identity})
 
-(defn root []
-  (let [text     (rf/subscribe [::text])
-        theme    (rf/subscribe [::theme])
-        language (rf/subscribe [::language])]
+
+(defn editor []
+  (let [ref (atom nil)]
     (fn []
       (r/create-class
-        {:name                "root"
-         :component-did-mount (fn [this]
-                                (let [el (r/dom-node this)]
-                                  (m/create-editor el {:value @text, :language @language, :theme @theme})))
-         :reagent-render      (fn []
-                                [:div#editor {:style {:height "100vh"}}])}))))
+        {:component-did-mount
+         (fn [this]
+           (m/create-editor (r/dom-node this) default-props))
+
+         :render
+         (fn []
+           [:div.editor-wrapper {:ref (fn [el]
+                                        (reset! ref el))}])}))))
+
+
+(defn root []
+  [:div.m-6
+   [:h1.text-gray-700 "Monaco Editor"]
+   [:div.mt-6.mb-10.flex.justify-start.w-full
+    [:div.mr-2
+     [:span.text-gray-700 "Theme:"]
+     [:select.form-select.mt-1.block
+      [:option "Light"]
+      [:option "Dark"]]]
+    [:div.mr-2
+     [:span.text-gray-700 "Language:"]
+     [:select.form-select.mt-1.block
+      [:option "Clojure"]]]]
+   [editor]])
 
 
 (defn mount-root
