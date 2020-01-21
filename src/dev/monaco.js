b0c022a57bc1a0dba423b0c5f35f9d46b83fe28f
Ilshat Sultanov <just.sultanov@gmail.com>
2020-01-21 15:21:56 +0300

    getWorkerUrl returns a relative path

--- src/dev/monaco.js
+++ src/dev/monaco.js
@@ -3,17 +3,17 @@ import * as monaco from "monaco-editor";
 self.MonacoEnvironment = {
   getWorkerUrl: function(moduleId, label) {
     if (label === "json") {
-      return "/js/json.worker.js";
+      return "./js/json.worker.js";
     }
     if (label === "css") {
-      return "/js/css.worker.js";
+      return "./js/css.worker.js";
     }
     if (label === "html") {
-      return "/js/html.worker.js";
+      return "./js/html.worker.js";
     }
     if (label === "typescript" || label === "javascript") {
-      return "/js/ts.worker.js";
+      return "./js/ts.worker.js";
     }
-    return "/js/editor.worker.js";
+    return "./js/editor.worker.js";
   }
 };
