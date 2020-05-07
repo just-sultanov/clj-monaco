38d6337ade8d6a51e4f1fbfcb8aabe1cd9dd108c
Ilshat Sultanov <just.sultanov@gmail.com>
2020-05-08 01:15:45 +0300

    Update example

--- src/dev/clojure/monaco/example.cljs
+++ src/dev/clojure/monaco/example.cljs
@@ -1,57 +1,72 @@
+;;;;
+;; Copyright © 2019-2020 Ilshat Sultanov. All rights reserved.
+;; The use and distribution terms for this software are covered by the license which
+;; can be found in the file license at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by the terms
+;; of this license. You must not remove this notice, or any other, from this software.
+;;;;
+
 (ns monaco.example
+  "A simple example of library usage."
   (:require
     [clojure.string :as str]
     [reagent.dom :as dom]
     [re-frame.core :as rf]
-    [monaco.core :as monaco]
-    [monaco.monarch :as monarch]
-    [monaco.helpers :as helpers]))
+    [monaco.core :as m]
+    [monaco.js-interop :as j]))
 
-;;
+;;;;
 ;; Helper functions
-;;
+;;;;
 
 (defn get-value [input]
-  (helpers/get-in input ["target" "value"]))
+  (j/get-in input [:target :value]))
 
 
 
-;;
+;;;;
 ;; Custom language
-;;
+;;;;
 
 (defn register! []
-  (monarch/register {:id "custom"})
+  (m/register {:id "custom"})
 
-  (monarch/set-monarch-tokens-provider "custom"
+  (m/set-monarch-tokens-provider "custom"
     {:tokenizer {:root [[#"\[error.*" "custom-error"]
                         [#"\[notice.*" "custom-notice"]
                         [#"\[info.*" "custom-info"]
                         [#"\[[a-zA-Z 0-9:]+\]" "custom-date"]]}})
 
-  (monarch/register-completion-item-provider "custom"
-    {:provideCompletionItems (fn []
-                               {:suggestions [{:label      "simpleText"
-                                               :insertText "simpleText"
-                                               :kind       (helpers/get-in monaco/monaco-editor ["CompletionItemKind" "Text"])}
-                                              {:label           "testing"
-                                               :insertText      "testing(${1:condition})"
-                                               :insertTextRules (helpers/get-in monaco/monaco-editor ["CompletionItemKind" "Keyword"])
-                                               :kind            (helpers/get-in monaco/monaco-editor ["CompletionItemInsertTextRule" "InsertAsSnippet"])}]})})
+  (m/register-completion-item-provider "custom"
+    {:provide-completion-items (fn []
+                                 {:suggestions [{:label       "simpleText"
+                                                 :insert-text "simpleText"
+                                                 :kind        (m/completion-item-kind :text)}
+                                                {:label             "testing"
+                                                 :insert-text       "testing(${1:condition})"
+                                                 :insert-text-rules (m/completion-item-kind :keyword)
+                                                 :kind              (m/completion-item-insert-text-rule :insert-as-snippet)}]})})
 
-  (monaco/define-theme "custom"
+  (m/define-theme "custom"
     {:base    "vs"
      :inherit false
      :rules   [{:token "custom-info" :foreground "808080"}
-               {:token "custom-error" :foreground "ff0000" :fontStyle "bold"}
+               {:token "custom-error" :foreground "ff0000" :font-style "bold"}
                {:token "custom-notice" :foreground "ffa500"}
                {:token "custom-date" :foreground "008800"}]}))
 
 
 
-;;
+;;;;
 ;; Events & subscriptions
-;;
+;;;;
+
+(def custom-text
+  "[Sun Mar 7 16:02:00 2004] [notice] Apache/1.3.29 (Unix) configured -- resuming normal operations
+[Sun Mar 7 16:02:00 2004] [info] Server built: Feb 27 2004 13:56:37
+[Sun Mar 7 16:02:00 2004] [notice] Accept mutex: sysvsem (Default: sysvsem)
+[Sun Mar 7 21:16:17 2004] [error] [client xx.xx.xx.xx] File does not exist: /home/httpd/twiki/view/Main/WebHome")
+
 
 (rf/reg-event-db
   ::init
@@ -61,23 +76,23 @@
                  "vs-dark"  "Dark"
                  "hc-black" "High Contrast"
                  "custom"   "Custom"}
-     :editor    {:width               "100%"
-                 :height              "100%"
-                 :value               "[Sun Mar 7 16:02:00 2004] [notice] Apache/1.3.29 (Unix) configured -- resuming normal operations\n[Sun Mar 7 16:02:00 2004] [info] Server built: Feb 27 2004 13:56:37\n[Sun Mar 7 16:02:00 2004] [notice] Accept mutex: sysvsem (Default: sysvsem)\n[Sun Mar 7 21:16:17 2004] [error] [client xx.xx.xx.xx] File does not exist: /home/httpd/twiki/view/Main/WebHome"
-                 :defaultValue        ""
-                 :language            "custom"
-                 :theme               "custom"
-                 :minimap             {:enabled true}
-                 :autoIndent          true
-                 :selectOnLineNumbers true
-                 :roundedSelection    false
-                 :readOnly            false
-                 :cursorStyle         "line"
-                 :automaticLayout     false
-                 :editorDidMount      (fn [editor monaco] (monaco/focus editor))
-                 :editorWillMount     (fn [monaco])
-                 :onChange            (fn [new-value event] (rf/dispatch [::set-value new-value]))
-                 :overrideServices    {}}}))
+     :editor    {:width                  "100%"
+                 :height                 "100%"
+                 :value                  custom-text
+                 :default-value          ""
+                 :language               "custom"
+                 :theme                  "custom"
+                 :minimap                {:enabled true}
+                 :auto-indent            true
+                 :select-on-line-numbers true
+                 :rounded-selection      false
+                 :read-only              false
+                 :cursor-style           "line"
+                 :automatic-layout       false
+                 :editor-did-mount       (fn [editor monaco] (m/focus editor))
+                 :editor-will-mount      (fn [monaco])
+                 :on-change              (fn [new-value event] (rf/dispatch [::set-value new-value]))
+                 :override-services      {}}}))
 
 (rf/reg-sub
   ::languages
@@ -169,13 +184,13 @@
   [:div.m-6
    [header]
    [config]
-   [monaco/editor @(rf/subscribe [::editor])]])
+   [m/editor @(rf/subscribe [::editor])]])
 
 
 
-;;
+;;;;
 ;; Application entry point
-;;
+;;;;
 
 (defn mount-root
   "Mount root component."
@@ -183,7 +198,7 @@
   []
   (rf/clear-subscription-cache!)
   (dom/render [root]
-    (helpers/call js/document "getElementById" "root")))
+    (j/invoke js/document :get-element-by-id "root")))
 
 
 (defn init
